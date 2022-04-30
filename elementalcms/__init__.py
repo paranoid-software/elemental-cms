3d8b25d8b9073fa47fe14e0c5d0de5aa8f08e22d
Pablo Pinargote <pablo.pinargote@outlook.com>
2022-04-30 08:56:14 -0500

    Versioning

--- elementalcms/__init__.py
+++ elementalcms/__init__.py
@@ -14,7 +14,7 @@ from elementalcms.admin import admin
 from elementalcms.presenter import presenter
 from elementalcms.identity import identity
 
-__version__ = "1.0.89"
+__version__ = "1.0.90"
 
 
 class Elemental:
