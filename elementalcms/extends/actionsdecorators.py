a5a0305e55d572214f4f855fdb34bf31141ed0b1
Pablo Pinargote <pablo.pinargote@outlook.com>
2022-05-18 11:07:20 -0500

    Removing gateway token decorator

--- elementalcms/extends/actionsdecorators.py
+++ elementalcms/extends/actionsdecorators.py
@@ -1,27 +1,3 @@
-import ctypes
-import json
-import os
-from functools import wraps
-from http import HTTPStatus
-
-from flask import current_app, Response, session, request
-
-
-def gateway_token_required(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        gateway_token = request.headers.get('X-Gateway-Token', None)
-        if gateway_token is None:
-            return Response(status=HTTPStatus.UNAUTHORIZED)
-        if current_app.config['USER_IDENTITY_SESSION_KEY'] not in session:
-            return Response(status=HTTPStatus.UNAUTHORIZED)
-        payload = json.dumps(session[current_app.config['USER_IDENTITY_SESSION_KEY']])
-        if gateway_token != ctypes.c_uint64(hash(payload)).value.to_bytes(8, "big").hex():
-            return Response(status=HTTPStatus.UNAUTHORIZED)
-        return f(*args, **kwargs)
-    return wrapper
-
-
 def get(route: str, **kwargs):
     def wrapper(function):
         function.is_action = True
